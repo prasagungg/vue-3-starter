9141088ad960f24bce4b4e44f026636a3f41798e
prasagungg <agungprasetio878@gmail.com>
2025-01-20 14:31:35 +0700

    feat: login page

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -5,6 +5,5 @@
     <div class="grid place-content-center h-full">
       <slot />
     </div>
-    <div class="bg-auth-header w-full h-[70vh] absolute bottom-0 bg-no-repeat bg-cover"></div>
   </div>
 </template>
--- src/modules/auth/pages/Login.vue
+++ src/modules/auth/pages/Login.vue
@@ -53,10 +53,30 @@ const handleSubmit = (values: LoginDTO) => {
 </script>
 
 <template>
-  <div class="bg-white p-4 rounded-xl sm:w-[370px] z-20 relative shadow-2xl">
-    <div class="grid gap-2 text-center">
-      <h3 class="text-3xl font-bold font-logo text-primary">Acquirer</h3>
-      <LoginForm @submit="handleSubmit" :isLoading="isLoading" />
+  <div
+    class="container relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0"
+  >
+    <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+      <div class="absolute inset-0 bg-zinc-900" />
+      <div class="relative z-20 flex items-center text-lg font-medium">BOT Template</div>
+
+      <img
+        src="@/assets/images/login.svg"
+        class="relative m-auto h-[301px] w-[1000px]"
+        alt="Vite"
+      />
+    </div>
+    <div class="lg:p-8">
+      <div class="mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[350px]">
+        <div class="flex flex-col space-y-2 text-left">
+          <h1 class="text-2xl font-semibold tracking-tight">Login</h1>
+          <p class="text-sm text-muted-foreground">
+            Enter your email and password below <br />
+            to log into your account
+          </p>
+        </div>
+        <LoginForm @submit="handleSubmit" :isLoading="isLoading" />
+      </div>
     </div>
   </div>
 </template>
